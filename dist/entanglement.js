9e4fc3479b4c808bb7c76c861815d1af242b67e5
Steve Baker <tektsu@gmail.com>
2020-07-26 15:24:43 -0700

    Ambler uses BoxSpiralElement

--- dist/entanglement.js
+++ dist/entanglement.js
@@ -701,6 +701,7 @@ class Aahs extends Tangle {
  * @property {Point} ne The northeast corner of the quadrilateral. Overrides size.
  * @property {Point} se The southeast corner of the quadrilateral. Overrides size.
  * @property {Point} sw The southwest corner of the quadrilateral. Overrides size.
+ * @property {boolean} interior If true, do not touch the sides of the quadrilateral, draw the spiral inside the quadrilateral with two fewer divisions than specified. The default is false.
  * @property {value} any Any of the TangleElementOptions may be used here.
  */
 
@@ -726,6 +727,7 @@ class BoxSpiralElement extends TangleElement {
             rotation: 'ccw',
             size: 50,
             startCorner: 'nw',
+            interior: false,
         };
         if (!('fillColor' in options)) {
             options.fillColor = color(0, 0, 0, 0);
@@ -735,7 +737,7 @@ class BoxSpiralElement extends TangleElement {
             this.divisions.max += 1;
             this.divisions = Math.floor(this.divisions.rand());
         }
-        this.divisions = Math.max(2, this.divisions);
+        this.divisions = Math.max(this.interior ? 3 : 2, this.divisions);
         if (this.rotation === 'random') {
             this.rotation = ['cw', 'ccw'][Math.floor(random(0, 2))];
         }
@@ -851,22 +853,22 @@ class BoxSpiralElement extends TangleElement {
         if (this.rotation === 'cw') {
             this.direction++; // if cw, the initial direction is 1.
         }
-        this.current = 0;       // Index of first point
-        this.step = 0;
-        this.levelCount = 3;    // We need thre strokes at the first level, 2 for each subsequent level
+        this.current = this.interior ? this.divisions + 2 : 0;       // Index of first point
+        this.step =  this.interior ? 2 : 0;
+        this.levelCount = 3;    // We need three strokes at the first level, 2 for each subsequent level
 
         // Modifications if the starting corner is other than nw
         switch(this.startCorner) {
             case 'ne':
-                this.current = this.divisions;
+                this.current = this.interior ? 2 * this.divisions : this.divisions;
                 this.direction += 3;
                 break;
             case 'se':
-                this.current = Math.pow(this.divisions+1, 2) - 1;
+                this.current = this.interior ? Math.pow(this.divisions, 2) + this.divisions - 2 : Math.pow(this.divisions+1, 2) - 1;
                 this.direction += 2;
                 break;
             case 'sw':
-                this.current = this.divisions * (this.divisions+1);
+                this.current = this.interior ? Math.pow(this.divisions, 2) : this.divisions * (this.divisions+1);
                 this.direction += 1;
                 break;
         }
@@ -929,101 +931,28 @@ class Ambler extends Tangle {
         if (typeof options === 'undefined') options = {};
         super(width, height, options);
 
-        this.grid();
+        this.buildGridPoints();
 
+        const starts = ['nw', 'sw', 'se', 'ne'];
         let colRotate = 0;
         let rowRotate = colRotate + 1;
-        for (let r=0; r<this.gridPoints.length-1; r++) {
-            for (let c=0; c<this.gridPoints[r].length-1; c++) {
-                const p = this._pointPool(this.gridPoints[r][c], this.gridPoints[r][c + 1],
-                    this.gridPoints[r + 1][c + 1], this.gridPoints[r + 1][c]);
-                let points = [];
-                const rotate = colRotate % 4;
-                switch(rotate) {    // Different rotations use different points from the pool
-                    case 0:
-                        points = [p[0], p[14], p[16], p[2], p[1], p[11], p[13], p[5], p[4], p[8]];
-                        break;
-                    case 1:
-                        points = [p[14], p[16], p[2], p[0], p[10], p[13], p[5], p[3], p[7], p[8]];
-                        break;
-                    case 2:
-                        points = [p[16], p[2], p[0], p[14], p[15], p[5], p[3], p[11], p[12], p[8]];
-                        break;
-                    case 3:
-                        points = [p[2], p[0], p[14], p[16], p[6], p[3], p[11], p[13], p[9], p[8]];
-                        break;
-                }
-                for (let i = 1; i < points.length; i++) {
-                    this.g.line(points[i-1].x , points[i-1].y, points[i].x, points[i].y);
-                }
+        for (let r = 0; r < this.gridPoints.length - 1; r++) {
+            for (let c = 0; c < this.gridPoints[r].length - 1; c++) {
+                const nw = this.gridPoints[r][c];
+                const ne = this.gridPoints[r][c + 1];
+                const se = this.gridPoints[r + 1][c + 1];
+                const sw = this.gridPoints[r + 1][c];
+                const bse = BoxSpiralElement.newFromCoordinates(this.g, nw, ne, se, sw, {
+                    startCorner: starts[colRotate % 4],
+                    divisions: 6,
+                    interior: true,
+                });
+                bse.draw();
                 colRotate++;
             }
             colRotate = rowRotate++;
         }
+
+        this.grid();
     }
-
-    /**
-     *  _pointPool() creates a grid of proportionally-spaced points inside a quadrilateral (q). The q is divided into
-     *  36 sections (6x6) and a list of interior points defining those sections is returned. There would be 25 such
-     *  points, but only 17 are needed to draw an Ambler spiral in all its rotations, so only those are calculated.
-     *
-     *      +--+--+--+--+--+--+
-     *      |                 |     This chart shows the 17 points calculated and the indexes in the
-     *      +  0  1  .  .  2  +     returned array which they occupy.
-     *      |                 |
-     *      +  .  3  4  5  6  +
-     *      |                 |
-     *      +  .  7  8  9  .  +
-     *      |                 |
-     *      + 10 11 12 13  .  +
-     *      |                 |
-     *      + 14  .  . 15  16 +
-     *      |                 |
-     *      +--+--+--+--+--+--+
-     *
-     *  _pointPool() takes 4 parameters, all of type Point:
-     *  the upper-left (northwest), upper-right (northeast),
-     *  lower-right (southeast), and lower-left (southwest)
-     *  points of the quadrilateral.
-     *
-     * @private
-     */
-    _pointPool(nw, ne, se, sw) {
-        const segments = 6;
-
-        // Create the interior lines
-        const lwyPoints = new Line(nw, sw).divide(segments);
-        const leyPoints = new Line(ne, se).divide(segments);
-        const lnxPoints = new Line(nw, ne).divide(segments);
-        const lsxPoints = new Line(sw, se).divide(segments);
-        let vLines = [];
-        let hLines = [];
-        for (let i=1; i<segments; i++) {
-            vLines.push(new Line(lnxPoints[i], lsxPoints[i]));
-            hLines.push(new Line(lwyPoints[i], leyPoints[i]));
-        }
-
-        // Create the point pool from which the spirals will be created, using intersections of interior lines
-        return  [
-            hLines[0].intersection(vLines[0]),
-            hLines[0].intersection(vLines[1]),
-            hLines[0].intersection(vLines[4]),
-            hLines[1].intersection(vLines[1]),
-            hLines[1].intersection(vLines[2]),
-            hLines[1].intersection(vLines[3]),
-            hLines[1].intersection(vLines[4]),
-            hLines[2].intersection(vLines[1]),
-            hLines[2].intersection(vLines[2]),
-            hLines[2].intersection(vLines[3]),
-            hLines[3].intersection(vLines[0]),
-            hLines[3].intersection(vLines[1]),
-            hLines[3].intersection(vLines[2]),
-            hLines[3].intersection(vLines[3]),
-            hLines[4].intersection(vLines[0]),
-            hLines[4].intersection(vLines[3]),
-            hLines[4].intersection(vLines[4]),
-        ];
-
-    }
-
 }
--- src/Ambler.js
+++ src/Ambler.js
@@ -17,101 +17,28 @@ class Ambler extends Tangle {
         if (typeof options === 'undefined') options = {};
         super(width, height, options);
 
-        this.grid();
+        this.buildGridPoints();
 
+        const starts = ['nw', 'sw', 'se', 'ne'];
         let colRotate = 0;
         let rowRotate = colRotate + 1;
-        for (let r=0; r<this.gridPoints.length-1; r++) {
-            for (let c=0; c<this.gridPoints[r].length-1; c++) {
-                const p = this._pointPool(this.gridPoints[r][c], this.gridPoints[r][c + 1],
-                    this.gridPoints[r + 1][c + 1], this.gridPoints[r + 1][c]);
-                let points = [];
-                const rotate = colRotate % 4;
-                switch(rotate) {    // Different rotations use different points from the pool
-                    case 0:
-                        points = [p[0], p[14], p[16], p[2], p[1], p[11], p[13], p[5], p[4], p[8]];
-                        break;
-                    case 1:
-                        points = [p[14], p[16], p[2], p[0], p[10], p[13], p[5], p[3], p[7], p[8]];
-                        break;
-                    case 2:
-                        points = [p[16], p[2], p[0], p[14], p[15], p[5], p[3], p[11], p[12], p[8]];
-                        break;
-                    case 3:
-                        points = [p[2], p[0], p[14], p[16], p[6], p[3], p[11], p[13], p[9], p[8]];
-                        break;
-                }
-                for (let i = 1; i < points.length; i++) {
-                    this.g.line(points[i-1].x , points[i-1].y, points[i].x, points[i].y);
-                }
+        for (let r = 0; r < this.gridPoints.length - 1; r++) {
+            for (let c = 0; c < this.gridPoints[r].length - 1; c++) {
+                const nw = this.gridPoints[r][c];
+                const ne = this.gridPoints[r][c + 1];
+                const se = this.gridPoints[r + 1][c + 1];
+                const sw = this.gridPoints[r + 1][c];
+                const bse = BoxSpiralElement.newFromCoordinates(this.g, nw, ne, se, sw, {
+                    startCorner: starts[colRotate % 4],
+                    divisions: 6,
+                    interior: true,
+                });
+                bse.draw();
                 colRotate++;
             }
             colRotate = rowRotate++;
         }
+
+        this.grid();
     }
-
-    /**
-     *  _pointPool() creates a grid of proportionally-spaced points inside a quadrilateral (q). The q is divided into
-     *  36 sections (6x6) and a list of interior points defining those sections is returned. There would be 25 such
-     *  points, but only 17 are needed to draw an Ambler spiral in all its rotations, so only those are calculated.
-     *
-     *      +--+--+--+--+--+--+
-     *      |                 |     This chart shows the 17 points calculated and the indexes in the
-     *      +  0  1  .  .  2  +     returned array which they occupy.
-     *      |                 |
-     *      +  .  3  4  5  6  +
-     *      |                 |
-     *      +  .  7  8  9  .  +
-     *      |                 |
-     *      + 10 11 12 13  .  +
-     *      |                 |
-     *      + 14  .  . 15  16 +
-     *      |                 |
-     *      +--+--+--+--+--+--+
-     *
-     *  _pointPool() takes 4 parameters, all of type Point:
-     *  the upper-left (northwest), upper-right (northeast),
-     *  lower-right (southeast), and lower-left (southwest)
-     *  points of the quadrilateral.
-     *
-     * @private
-     */
-    _pointPool(nw, ne, se, sw) {
-        const segments = 6;
-
-        // Create the interior lines
-        const lwyPoints = new Line(nw, sw).divide(segments);
-        const leyPoints = new Line(ne, se).divide(segments);
-        const lnxPoints = new Line(nw, ne).divide(segments);
-        const lsxPoints = new Line(sw, se).divide(segments);
-        let vLines = [];
-        let hLines = [];
-        for (let i=1; i<segments; i++) {
-            vLines.push(new Line(lnxPoints[i], lsxPoints[i]));
-            hLines.push(new Line(lwyPoints[i], leyPoints[i]));
-        }
-
-        // Create the point pool from which the spirals will be created, using intersections of interior lines
-        return  [
-            hLines[0].intersection(vLines[0]),
-            hLines[0].intersection(vLines[1]),
-            hLines[0].intersection(vLines[4]),
-            hLines[1].intersection(vLines[1]),
-            hLines[1].intersection(vLines[2]),
-            hLines[1].intersection(vLines[3]),
-            hLines[1].intersection(vLines[4]),
-            hLines[2].intersection(vLines[1]),
-            hLines[2].intersection(vLines[2]),
-            hLines[2].intersection(vLines[3]),
-            hLines[3].intersection(vLines[0]),
-            hLines[3].intersection(vLines[1]),
-            hLines[3].intersection(vLines[2]),
-            hLines[3].intersection(vLines[3]),
-            hLines[4].intersection(vLines[0]),
-            hLines[4].intersection(vLines[3]),
-            hLines[4].intersection(vLines[4]),
-        ];
-
-    }
-
 }
